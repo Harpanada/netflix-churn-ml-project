ce8af8d6ec6de4ecaf7aa689acdf6608cb0f23cd
Harpanada <liyovanh@gmail.com>
2026-04-26 18:53:38 +0700

    feat(preprocessing): Drop unused columns and remove outliers

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -15,3 +15,41 @@ X_train,X_test,y_train,y_test= train_test_split(X,y,test_size=0.2, random_state=
 
 
 
+
+#Remove useless data
+drop_cols= ['customer_id']
+X_train=X_train.drop(columns=drop_cols)
+X_test=X_test.drop(columns=drop_cols)
+
+#Remove unreasonable data
+mask_train= X_train["avg_watch_time_per_day"] <= 24
+mask_test=  X_test["avg_watch_time_per_day"]  <= 24
+
+X_train=X_train[mask_train]
+y_train=y_train[mask_train]
+
+X_test=X_test[mask_test]
+y_test=y_test[mask_test]
+
+#Separate numeric and categorycal data
+numeric_col= X_train.select_dtypes(include=["number"]).columns
+categ_col= X_train.select_dtypes(include=['object', 'str']).columns
+
+#Drop unused columns
+drop_cols= ['customer_id']
+X_train=X_train.drop(columns=drop_cols)
+X_test=X_test.drop(columns=drop_cols)
+
+#Remove outliers 
+mask_train= X_train["avg_watch_time_per_day"] <= 24
+mask_test=  X_test["avg_watch_time_per_day"]  <= 24
+
+X_train=X_train[mask_train]
+y_train=y_train[mask_train]
+
+X_test=X_test[mask_test]
+y_test=y_test[mask_test]
+
+#Separate numeric and categorycal data
+numeric_col= X_train.select_dtypes(include=["number"]).columns
+categ_col= X_train.select_dtypes(include=['object', 'str']).columns
