1f535235b10b2eb688cd6366679dc110219c6e80
Harpanada <liyovanh@gmail.com>
2026-04-26 18:50:50 +0700

    feat(data): split dataset into train and test sets

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,20 +3,15 @@ from sklearn.preprocessing import OneHotEncoder,StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 
+#Loading Dirty Dataset
 df=pd.read_csv('data/raw/netflix_customer_churn.csv')
 
+#Separate label and features
 X= df.drop(columns='churned')
 y= df['churned']
 
+#Split data to test and train
 X_train,X_test,y_train,y_test= train_test_split(X,y,test_size=0.2, random_state=42)
 
-drop_cols= ['customer_id']
-X_train=X_train.drop(columns=drop_cols)
-X_test=X_test.drop(columns=drop_cols)
-
-numeric_col= X_train.select_dtypes(include=["int64","float64"]).columns
-categ_col= X_train.select_dtypes(include=['object', 'str']).columns
 
 
-
- 
