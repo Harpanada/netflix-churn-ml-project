79570b8b041bdc673e7239030b4d0434056162c5
Harpanada <liyovanh@gmail.com>
2026-04-27 20:16:20 +0700

    feat(train): Build base model.

--- src/train.py
+++ src/train.py
@@ -2,3 +2,13 @@ from sklearn.linear_model import LogisticRegression
 from preprocessing import get_data_and_preprocessor
 from sklearn.pipeline import Pipeline
 
+#Get data and preprocessor
+X_train, X_test, y_train, y_test, preprocessor = get_data_and_preprocessor()
+
+#Build Models
+models={
+    "base_logreg":Pipeline([
+        ('prep',preprocessor),
+        ('clf',LogisticRegression())
+    ]),
+}
