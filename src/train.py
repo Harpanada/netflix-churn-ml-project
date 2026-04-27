1e13569ec468271641e05ce36ca0ee1681217904
Harpanada <liyovanh@gmail.com>
2026-04-27 20:38:45 +0700

    feat(train): Train and test base model.

--- src/train.py
+++ src/train.py
@@ -1,5 +1,7 @@
 from sklearn.linear_model import LogisticRegression
 from preprocessing import get_data_and_preprocessor
+from sklearn.metrics import classification_report,confusion_matrix
+from sklearn.model_selection import cross_val_score
 from sklearn.pipeline import Pipeline
 
 #Get data and preprocessor
@@ -7,8 +9,22 @@ X_train, X_test, y_train, y_test, preprocessor = get_data_and_preprocessor()
 
 #Build Models
 models={
-    "base_logreg":Pipeline([
+    "base_log_reg":Pipeline([
         ('prep',preprocessor),
         ('clf',LogisticRegression())
     ]),
 }
+
+#Train and test models
+for name,model in models.items():
+    model.fit(X_train,y_train)
+    y_pred=model.predict(X_test)
+    print(f"""Model: {name}
+          \nScore: {model.score(X_test,y_test):,.2f}
+          \nClassification Report:
+          \n{classification_report(y_test,y_pred)}
+          \nConfusion Matrix:
+          \n{confusion_matrix(y_test,y_pred)}
+          \nCross Val:
+          \n{cross_val_score(model, X_train, y_train, cv=5)}
+    """)
