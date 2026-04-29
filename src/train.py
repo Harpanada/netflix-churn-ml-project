3f26cef265946c23bc16cd161c6c767c6cde48d3
Harpanada <liyovanh@gmail.com>
2026-04-29 19:26:31 +0700

    feat(train): Add other models to compare and chose the best one.

--- src/train.py
+++ src/train.py
@@ -1,8 +1,12 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 from preprocessing import get_data_and_preprocessor
-from sklearn.metrics import classification_report,confusion_matrix
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score,precision_score,recall_score,f1_score
 from sklearn.model_selection import cross_val_score
 from sklearn.pipeline import Pipeline
+import pandas as pd
+import joblib
+
 
 #Get data and preprocessor
 X_train, X_test, y_train, y_test, preprocessor = get_data_and_preprocessor()
@@ -13,18 +17,48 @@ models={
         ('prep',preprocessor),
         ('clf',LogisticRegression())
     ]),
+    "Dec_tree":Pipeline([
+        ('prep',preprocessor),
+        ('Dec_tre',DecisionTreeClassifier())
+
+    ])
 }
 
 #Train and test models
+result=[]
+best_acc  = 0
+best_prec   = 0  
+best_recl = 0
+best_f1= 0
+best_model = None
+best_name  = None
 for name,model in models.items():
     model.fit(X_train,y_train)
     y_pred=model.predict(X_test)
-    print(f"""Model: {name}
-          \nScore: {model.score(X_test,y_test):,.2f}
-          \nClassification Report:
-          \n{classification_report(y_test,y_pred)}
-          \nConfusion Matrix:
-          \n{confusion_matrix(y_test,y_pred)}
-          \nCross Val:
-          \n{cross_val_score(model, X_train, y_train, cv=5)}
-    """)
+
+    accuration= accuracy_score(y_test,y_pred)
+    precision=  precision_score(y_test,y_pred)
+    recall=     recall_score(y_test,y_pred)
+    f1=         f1_score(y_test,y_pred)
+
+    result.append({
+        'Model'       : name,
+        'Accuration'  : accuration,
+        'Precision'   : precision,
+        'Recall'      : recall,
+        'f1_score'    :f1
+    })
+
+    if accuration > best_acc or precision > best_prec or recall > best_recl or f1 > best_f1:
+        best_acc        = accuration
+        best_prec       = precision
+        best_recl       = recall
+        best_f1         = f1
+        best_model      = model
+        best_name       = name
+
+result_df=pd.DataFrame(result)
+result_df=result_df.sort_values('f1_score',ascending=False)
+
+print(result_df)
+print(f"\n🏆 Best Model : {best_name}")
