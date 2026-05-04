3abd443f531ecd1f90696de083bdfe4f21abe3f3
Harpanada <liyovanh@gmail.com>
2026-05-04 12:28:52 +0700

    feat(main): Create predict function.

--- src/main.py
+++ src/main.py
@@ -6,6 +6,26 @@ model=joblib.load('../models/Trained_Dec_tree.pkl')
 app = Flask(__name__)
 
 
+@app.get("/")
+def root():
+   return render_template('index.html')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+   raw_input=request.json
+   
+   df=pd.DataFrame([raw_input])
+
+   result=model.predict(df)
+   pred=result[0]
+    
+   if pred == 0:
+      pred= 'Not Churned'
+   elif pred == 1:
+      pred= 'Churned'
+
+   return jsonify ({
+      'prediction':pred
+      })
 
 
